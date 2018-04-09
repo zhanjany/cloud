c474e794d10fb2585b095ffd3658982f3d5120ef
liuxiangtao <liuxingtao@xiaomabank.com>
2018-04-09 17:01:54 +0800

    fixbug

---
+++ cloud-auth/src/main/java/com/auth/web/HelloController.java
@@ -0,0 +1,42 @@
+package com.auth.web;
+
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+/**
+ * <p>
+ * ---<br>
+ * </p>
+ * <p>
+ * -----版本-----变更日期-----责任人-----变更内容<br>
+ * ─────────────────────────────────────<br>
+ * V3.0.0 2018/4/8 liuxiangtao 初版<br>
+ *
+ * @since XMJR V3.0.0
+ * </p>
+ */
+@RestController
+public class HelloController {
+
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() {
+        List<ServiceInstance> instances = client.getInstances("auth");
+        for (int i = 0; i < instances.size(); i++) {
+            logger.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
+        }
+        return "Hello World";
+    }
+}
